def763ebc30779ecf3f8677f8cf4529945f2f371
dennismaricarl <dennismaricarl@gmail.com>
2024-11-05 15:18:51 -0800

    added responsive design on etsy component

--- src/components/Etsy.jsx
+++ src/components/Etsy.jsx
@@ -2,7 +2,7 @@ const Etsy = () => {
     return (
         <div className="justify-center items-center flex flex-col h-screen hover:opacity-25" id="etsyId">
             <a href="https://www.etsy.com/shop/SunshineLoverStudios">
-                <h1 className="font-serif text-8xl text-pretty">SunshineLoverStudios</h1>
+                <h1 className="font-serif text-3xl lg:text-8xl">SunshineLoverStudios</h1>
             </a>
         </div>
     )
